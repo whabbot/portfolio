01cabd8efb1d2fee3123615458d21525305ee9f2
whabbot <whabbot0@gmail.com>
2026-03-15 01:18:28 +0000

    Add sanity app id

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,13 +3,14 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '8t2zlu0f',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
+    appId: 'j4mbxly7zsdhnhg3l7t6ndml',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-  }
+  },
 })
